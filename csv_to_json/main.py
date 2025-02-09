3e7ed46e464652c29b7f5096882dee832acfb97f
Andy8787(布長) <andy8787main@gmail.com>
2025-02-10 00:07:25 +0800

    demo_data

--- csv_to_json/main.py
+++ csv_to_json/main.py
@@ -4,19 +4,19 @@ import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-csv_path =  current_dir + "\Department_Intro.csv"
-json_path =  current_dir + "\Department_Intro.json"
+csv_path =  current_dir + "\\" + 'temp_data.csv'
+json_path =  current_dir + "\\" + 'temp_data.json'
 
 
 def csv_to_json(csv_file, json_file):
     data = []
     
-    with open(csv_file, mode='r', encoding='utf-8') as file:
+    with open(csv_file, mode='r', encoding='latin1') as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
     
-    with open(json_file, mode='w', encoding='utf-8') as file:
+    with open(json_file, mode='w', encoding='latin1') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
     
     print(f"Successfully converted {csv_file} to {json_file}")

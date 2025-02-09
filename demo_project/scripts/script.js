4670afaadcd6ddf68df302e924bb9b80d3cd277d
Andy8787(布長) <andy8787main@gmail.com>
2025-02-10 00:40:47 +0800

    移除判斷Website Link的長度

--- demo_project/scripts/script.js
+++ demo_project/scripts/script.js
@@ -6,10 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
             tbody.innerHTML = ""; 
 
             data.forEach(item => {
-                let urlText = item.URL;
-                // 如果 URL 超過 50 個字，就截斷並加上省略號
-                let displayText = urlText.length > 50 ? urlText.substring(0, 50) + "..." : urlText;
-
                 tbody.innerHTML += 
                 `<tr>
                     <td>${item.School_name}</td>

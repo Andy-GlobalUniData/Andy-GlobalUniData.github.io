928b72a48ef655e30d9e013cdf6475f5082bcc96
Andy8787(布長) <andy8787main@gmail.com>
2025-02-11 16:41:08 +0800

    輸出多個學校

--- demo_small_project/scripts/script.js
+++ demo_small_project/scripts/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("https://andy-globalunidata.github.io/demo_small_project/data/merged_output.json")
+    fetch("https://andy-globalunidata.github.io/demo_small_project/data/data.json")
         .then(response => response.json())
         .then(data => {
             const tbody = document.querySelector("#json-table tbody");

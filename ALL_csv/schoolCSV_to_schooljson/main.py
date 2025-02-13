902773cb7002587ad4f5feea77ff6a37a6a7cb82
Andy8787(布長) <andy8787main@gmail.com>
2025-02-13 14:28:18 +0800

    9個學校的數據

--- ALL_csv/schoolCSV_to_schooljson/main.py
+++ ALL_csv/schoolCSV_to_schooljson/main.py
@@ -13,12 +13,12 @@ School_name = "Bath Spa University"
 def csv_to_json(csv_file, json_file):
     data = []
     
-    with open(csv_file, mode='r', encoding='latin1') as file:
+    with open(csv_file, mode='r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
     
-    with open(json_file, mode='w', encoding='latin1') as file:
+    with open(json_file, mode='w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
     
     print(f"Successfully converted {csv_file} to {json_file}")
